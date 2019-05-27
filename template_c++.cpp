51b115a539cc80e3ac9186da095ea89241903eba
thesmallstar <manthan.surkar@gmail.com>
2019-05-27 21:16:56 +0530

    template added

---
+++ template_c++.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+# define ll long long int
+// manthan surkar  ((smallstar)) 
+						
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t;
+        cin>>t;
+
+	while(t--){
+	}
+   
+}
+	
