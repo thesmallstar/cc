51a3c87d407b84052b2ca880d898d5c9a7703757
Manthan Surkar <42006277+thesmallstar@users.noreply.github.com>
2019-07-17 21:32:21 +0530

    Create code2.cpp

---
+++ code2.cpp
@@ -0,0 +1,26 @@
+//SAAMPLE CODE 2 TO SUBMIT
+
+#include <bits/stdc++.h>
+using namespace std;
+# define ll long long int
+// manthan surkar  ((smallstar)) 
+						
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	ll n,k;
+
+        cin>>n;
+        cin>>k;
+        
+  
+     
+     cout<<n-((-3+sqrt(9+8*(n+k)))/2);
+     
+
+    
+
+
+   
+}
