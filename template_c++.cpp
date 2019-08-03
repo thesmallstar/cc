178f28274824ec9b12d3ac0f82d7d44237be95c6
Manthan Surkar <42006277+thesmallstar@users.noreply.github.com>
2019-08-03 21:52:18 +0530

    Update template_c++.cpp

--- template_c++.cpp
+++ template_c++.cpp
@@ -1,18 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
-# define ll long long int
 
-// manthan surkar  ((smallstar)) 
-						
+
+//************************************//
+// Manthan Surkar----((smallstar))----// 
+//***********************************//
+
+
+/*
+ * You may think you know what the following code does.
+ * But you dont. Trust me.
+ * Fiddle with it, and youll spend many a sleepless
+ * night cursing the moment you thought youd be clever
+ * enough to "optimize" the code below.
+ * Now close this file and go play with something else.
+ */
+
+
+//some useful macros
+# define ll long long int
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+//
+
+// iteration macros: have advantage of auto-casting and not recomputing arguments
+#define rep(i,n) for(int i=0, _##i=(n); i<_##i; ++i)
+#define dwn(i,n) for(int i=(n); --i>=0; )
+#define repr(i,l,r) for(int i=(l), _##i=(r); i<_##i; ++i)
+#define dwnr(i,l,r) for(int i=(r), _##i=(l); --i>=_##i; )
+#define repi(i,a) for(__typeof((a).begin()) i=(a).begin(), _##i=(a).end(); i!=_##i; ++i)
+#define dwni(i,a) for(__typeof((a).rbegin()) i=(a).rbegin(), _##i=(a).rend(); i!=_##i; ++i)
+//
+
 
 int main(){
 	ios::sync_with_stdio(false);
 	cin.tie(0);
+	
 	int t;
+
         cin>>t;
 
-	while(t--){
-	}
+	  rep(i,t){
+
+
+
+         }
    
 }
 	
