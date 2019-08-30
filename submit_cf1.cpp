8e64bb309b05f955a290a9289c42857d9a3cced3
Manthan Surkar <42006277+thesmallstar@users.noreply.github.com>
2019-08-30 21:20:19 +0530

    Create submit_cf1.cpp

---
+++ submit_cf1.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+//************************************//
+//-Manthan Surkar----((smallstar))----//
+//***********************************//
+/*
+ * You may think you know what the following code does.
+ * But you dont. Trust me.
+ * Fiddle with it, and youll spend many a sleepless
+ * night cursing the moment you thought youd be clever
+ * enough to "optimize" the code below.
+ * Now close this file and go play with something else.
+ */
+//***************************************************************************
+#define ll long long int
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+//****************************************************************************
+#define rep(i,n) for(int i=0, _##i=(n); i<_##i; ++i)
+#define dwn(i,n) for(int i=(n); --i>=0; )
+#define repr(i,l,r) for(int i=(l), _##i=(r); i<_##i; ++i)
+#define dwnr(i,l,r) for(int i=(r), _##i=(l); --i>=_##i; )
+#define repi(i,a) for(__typeof((a).begin()) i=(a).begin(), _##i=(a).end(); i!=_##i; ++i)
+#define dwni(i,a) for(__typeof((a).rbegin()) i=(a).rbegin(), _##i=(a).rend(); i!=_##i; ++i)
+//******************************************************************************
+
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+     map <int,int> a;
+     int max=0;
+     int t;
+     cin>>t;
+     int j;
+     for(int i=0;i<t;i++){
+        int l;
+        cin>>l;
+        a[l]++;
+        if(a[l]>max){
+          max=a[l];
+          j=l;
+        }
+        
+     }
+     long long ans=0;
+   repi(i,a)
+         {
+            if((j-i->first)%2!=0){
+                ans+= i->second;
+            }
+         
+         
+     }
+       
+cout<<ans;
+
+
+
+
+
+
+
+}
+
+
+//************************************//
+// Manthan Surkar----((smallstar))----//
+//***********************************//
