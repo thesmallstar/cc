abbe58821174000e491e72c867fc2fa0df221b53
Manthan Surkar <42006277+thesmallstar@users.noreply.github.com>
2019-09-08 13:25:16 +0530

    Update template_c++.cpp

--- template_c++.cpp
+++ template_c++.cpp
@@ -1,22 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-//************************************//
-//-Manthan Surkar----((smallstar))----//
-//***************************************************************************
+                           //-(smallstar))----
 #define fastio ios::sync_with_stdio(false); cin.tie(0);
 #define ll long long int
 #define mp make_pair
 #define pb push_back
 #define fi first
 #define se second
-//****************************************************************************
 #define rep(i,n) for(int i=0, _##i=(n); i<_##i; ++i)
 #define dwn(i,n) for(int i=(n); --i>=0; )
 #define repr(i,l,r) for(int i=(l), _##i=(r); i<_##i; ++i)
 #define dwnr(i,l,r) for(int i=(r), _##i=(l); --i>=_##i; )
 #define repi(i,a) for(__typeof((a).begin()) i=(a).begin(), _##i=(a).end(); i!=_##i; ++i)
 #define dwni(i,a) for(__typeof((a).rbegin()) i=(a).rbegin(), _##i=(a).rend(); i!=_##i; ++i)
-//******************************************************************************
+
 
 int main(){
 	fastio
